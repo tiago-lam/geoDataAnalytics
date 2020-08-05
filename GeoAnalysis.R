ade7969e4296f381f6365de3bb1f84918ff379d0
Tiago-LA-Machado <tiago.machado@nyu.edu>
2020-08-05 16:04:09 -0400

    analysis across scenarios

--- GeoAnalysis.R
+++ GeoAnalysis.R
@@ -1,5 +1,6 @@
 #packages
 install.packages("ggplot2")
+install.packages("plyr")
 library(ggplot2)
 library(scales)
 library(readr)
@@ -15,21 +16,28 @@ coneBreak <- geoExData$Sc1_1_conebreaking
 soilType_correct <-geoExData$Sc1_1_soiltype_correct
 scene_11 <- data.frame(school, semester, manual, stopDepth, coneBreak)
 
+
+###############################################################################
+#                                                                             #
+#                           ANALYSIS                                          #
+#                                                                             #
+###############################################################################
+
 #does automatic_manual influences cone breaking (from scene 1.1 to 2.3)
-mcnemarTestCone(school, semester, manual, coneBreak)
-mcnemarTestCone(school, semester, manual, test = geoExData$Sc1_2_conebreaking)
-mcnemarTestCone(school, semester, manual, test = geoExData$Sc1_3_conebreaking)
-mcnemarTestCone(school, semester, manual, test = geoExData$Sc2_1_conebreaking)
-mcnemarTestCone(school, semester, manual, test = geoExData$Sc2_2_conebreaking)
-mcnemarTestCone(school, semester, manual, test = geoExData$Sc2_3_conebreaking)
+mcnemarTestCone(manual, coneBreak)
+mcnemarTestCone(manual, test = geoExData$Sc1_2_conebreaking)
+mcnemarTestCone(manual, test = geoExData$Sc1_3_conebreaking)
+mcnemarTestCone(manual, test = geoExData$Sc2_1_conebreaking)
+mcnemarTestCone(manual, test = geoExData$Sc2_2_conebreaking)
+mcnemarTestCone(manual, test = geoExData$Sc2_3_conebreaking)
 
 # influence of manual vs automatic on soil type identification (from scene 1.1 to 2.3)
-mcnemarTestSoilType(school, semester, manual, soilType_correct)
-mcnemarTestSoilType(school, semester, manual, test = geoExData$Sc1_2_soiltype_correct)
-mcnemarTestSoilType(school, semester, manual, test = geoExData$Sc1_3_soiltype_correct)
-mcnemarTestSoilType(school, semester, manual, test = geoExData$Sc2_1_soiltype_correct)
-mcnemarTestSoilType(school, semester, manual, test = geoExData$Sc2_2_soiltype_correct)
-mcnemarTestSoilType(school, semester, manual, test = geoExData$Sc2_3_soiltype_correct)
+mcnemarTestSoilType(manual, soilType_correct)
+mcnemarTestSoilType(manual, test = geoExData$Sc1_2_soiltype_correct)
+mcnemarTestSoilType(manual, test = geoExData$Sc1_3_soiltype_correct)
+mcnemarTestSoilType(manual, test = geoExData$Sc2_1_soiltype_correct)
+mcnemarTestSoilType(manual, test = geoExData$Sc2_2_soiltype_correct)
+mcnemarTestSoilType(manual, test = geoExData$Sc2_3_soiltype_correct)
 
 #difference of stop depths given manual or automatic (from scene 1.1 to 2.3)
 generalTests(scene_11$manual, scene_11$stopDepth, "two.sided")
@@ -55,13 +63,82 @@ generalTests(geoExData$Sc2_1_manual, geoExData$Sc2_1_analysistotal, "two.sided")
 generalTests(geoExData$Sc2_2_manual, geoExData$Sc2_2_analysistotal, "two.sided")
 generalTests(geoExData$Sc2_3_manual, geoExData$Sc2_3_analysistotal, "two.sided")
 
+#medians, means and sds from stop-depth across scenarios
+getMedianAndSDFrom(geoExData$Sc1_1_stoppingdepth)
+getMedianAndSDFrom(geoExData$Sc1_2_stoppingdepth)
+getMedianAndSDFrom(geoExData$Sc1_3_stoppingdepth)
+getMedianAndSDFrom(geoExData$Sc2_1_stoppingdepth)
+getMedianAndSDFrom(geoExData$Sc2_2_stoppingdepth)
+getMedianAndSDFrom(geoExData$Sc2_3_stoppingdepth)
+
+#medians, means and sds from CPT total across scenarios
+getMedianAndSDFrom(geoExData$Sc1_1_CPTtotal)
+getMedianAndSDFrom(geoExData$Sc1_2_CPTtotal)
+getMedianAndSDFrom(geoExData$Sc1_3_CPTtotal)
+getMedianAndSDFrom(geoExData$Sc2_1_CPTtotal)
+getMedianAndSDFrom(geoExData$Sc2_2_CPTtotal)
+getMedianAndSDFrom(geoExData$Sc2_3_CPTtotal)
+
+#medians, means and sds from analysis across scenarios
+
+getMedianAndSDFrom(geoExData$Sc1_1_analysistotal)
+getMedianAndSDFrom(geoExData$Sc1_2_analysistotal)
+getMedianAndSDFrom(geoExData$Sc1_3_analysistotal)
+getMedianAndSDFrom(geoExData$Sc2_1_analysistotal)
+getMedianAndSDFrom(geoExData$Sc2_2_analysistotal)
+getMedianAndSDFrom(geoExData$Sc2_3_analysistotal)
+
+analysisScore <- cbind(geoExData$Sc1_1_analysistotal)
+analysisScore <- append(analysisScore, geoExData$Sc1_2_analysistotal)
+analysisScore <- append(analysisScore, geoExData$Sc1_3_analysistotal)
+analysisScore <- append(analysisScore, geoExData$Sc2_1_analysistotal)
+analysisScore <- append(analysisScore, geoExData$Sc2_2_analysistotal)
+analysisScore <- append(analysisScore, geoExData$Sc2_3_analysistotal)
+
+cptScore <- cbind(geoExData$Sc1_1_CPTtotal)
+cptScore <- append(cptScore, geoExData$Sc1_2_CPTtotal)
+cptScore <- append(cptScore, geoExData$Sc1_3_CPTtotal)
+cptScore <- append(cptScore, geoExData$Sc2_1_CPTtotal)
+cptScore <- append(cptScore, geoExData$Sc2_2_CPTtotal)
+cptScore <- append(cptScore, geoExData$Sc2_3_CPTtotal)
+
+group <- rep("scene_1_1", 251)
+group <- append(group, rep("scene_1_2", 251))
+group <- append(group, rep("scene_1_3", 251))
+group <- append(group, rep("scene_2_1", 251))
+group <- append(group, rep("scene_2_2", 251))
+group <- append(group, rep("scene_2_3", 251))
+
+groupAnalysisScore <- data.frame(group, cptScore, analysisScore)
+#analysis across scenarios for total analysis
+kruskal.test(groupAnalysisScore$analysisScore ~ groupAnalysisScore$group)#no diff
+#analysis across scenarios for cpt analysis
+kruskal.test(groupAnalysisScore$cptScore ~ groupAnalysisScore$group)#has diff
+
+
 ###############################################################################
 #                                                                             #
 #                           FUNCTIONS                                         #
 #                                                                             #
 ###############################################################################
 
-mcnemarTestCone <- function(school, semester, manual, test)
+getMedianAndSDFrom <- function(scenario)
+{
+  m = as.matrix(scenario)
+  m<-m[-which(is.na(m)),]
+  m <- m[m != "None"]
+  m <- sapply(m, as.numeric)
+  md <- (median(m))
+  mn <- mean(m)
+  sdev <- sd(m)
+  mtx <- matrix(nrow = 1, ncol = 3) 
+  mtx[1,1] <- md
+  mtx[1,2] <- mn
+  mtx[1,3] <- sdev
+  return (mtx)
+}
+
+mcnemarTestCone <- function(manual, test)
 {
   ## categorizing
   test[test=="TRUE"] <- "1"
@@ -82,7 +159,7 @@ mcnemarTestCone <- function(school, semester, manual, test)
   mcnemar.test(contingency, correct = FALSE)
 }
 
-mcnemarTestSoilType <- function(school, semester, manual, test)
+mcnemarTestSoilType <- function(manual, test)
 {
   "framing"
   autoManual_soilCorrect = data.frame(autoVsManual, test)
@@ -116,3 +193,13 @@ generalTests <- function(manual, varToTest, alt)
   manualVector <- as.numeric(as.matrix(manual$varToTest))
   t.test(autoVector, manualVector, alternative = alt)
 }
+
+###############################################################################
+#                                                                             #
+#                           just testing stuff                                #
+#                                                                             #
+###############################################################################
+
+stop <- geoExData$Sc1_1_stoppingdepth
+stop <- stop[-which(is.na(stop))]
+stop <- stop[stop != 'None']
